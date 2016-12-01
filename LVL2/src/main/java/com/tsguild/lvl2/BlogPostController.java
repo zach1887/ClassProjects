d6b513f9f2235056c78f2ff566eb10a69dbb3ec0
Jesse Cunningham <zach92indy@gmail.com>
2016-12-01 15:45:39 -0500

    Blog Controller update

--- LVL2/src/main/java/com/tsguild/lvl2/BlogPostController.java
+++ LVL2/src/main/java/com/tsguild/lvl2/BlogPostController.java
@@ -1,9 +1,9 @@
-
 package com.tsguild.lvl2;
 
 import com.tsguild.lvl2.dao.BlogPostDao;
 import com.tsguild.lvl2.dto.BlogPost;
 import com.tsguild.lvl2.dto.Comment;
+import java.util.List;
 import javax.inject.Inject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -24,24 +24,30 @@ public class BlogPostController {
     public BlogPostController(BlogPostDao dao) {
         this.dao = dao;
     }
-    
+
     @RequestMapping(value = "/blog/{id}", method = RequestMethod.GET)
     public String displayBlog(ModelMap model, @PathVariable("id") int id) {
         BlogPost post = dao.getBlogPostById(id);
-        
+
         model.addAttribute("title", post.getTitle());
         model.addAttribute("author", post.getAuthor());
         model.addAttribute("datePosted", post.getDatePosted());
         model.addAttribute("content", post.getContent());
-        
+
         return "template/blog";
     }
-    
-    public String displayBlogComments(int id) {
-        throw new UnsupportedOperationException("nope");
+
+    @RequestMapping(value = "/{blogId}", method = RequestMethod.GET)
+    public void displayBlogComments(@PathVariable int postId) {
+        dao.loadCommentsByBlogId(postId);
     }
-    
-    public void createComment(Comment comment, int id) {
-        throw new UnsupportedOperationException("nope");
+
+    @RequestMapping(value = "/comment", method = RequestMethod.POST)
+    public void createComment(Comment comment) {
+        dao.createComment(comment);
+    }
+    @RequestMapping(value = "/comment/{commentId}", method = RequestMethod.DELETE)
+    public void deleteComment(@PathVariable int commentId) {
+        dao.deleteComment(commentId);
     }
 }
